aa3158be51b4fcf2c761a61f5b86693ef7ef66e0
thehub97 <theresa.huber9@gmail.com>
2026-06-28 12:54:21 -0400

    Add Flask web server

--- server.py
+++ server.py
@@ -11,6 +11,9 @@ def emot_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
